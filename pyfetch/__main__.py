5011d8c0d4e27acec82c483999a34058974fe970
ehnoeck <zaakeenock@gmail.com>
2024-02-07 17:07:21 +0530

    md

--- pyfetch/__main__.py
+++ pyfetch/__main__.py
@@ -2,19 +2,22 @@ import argparse
 
 from pyfetch.fetch import Fetcher
 
-Fetcher.fetch()
+# Fetcher.fetch()
 
 def parse_args():
-    ap = argparse.ArgumentParser(allow_abbrev=False)
-    ap.add_argument(
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument(
         '-f',
         "--file_path",
         type=str,
         help="Request filepath of type .yaml , .yml or .json"
     )
-    return ap.parse_args()
+    return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    
+    print(args)
+
+if __name__ == "__main__":
+    main()    
